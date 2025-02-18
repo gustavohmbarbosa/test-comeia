d9c3f83c6dd6820d7c47b8643c61526452f28f5e
Gustavo Barbosa <gustavobarbosa-18@hotmail.com>
2025-02-18 15:32:00 -0300

    refactor: remover itens ainda nao implementados

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import 'dotenv/config'
 import { routes } from './routes';
-import { globalExpressHandle } from './erros/global-express-handle';
 import { connectToMongoDB } from './config/mongodb';
 
 process.on("uncaughtException", (err) => {
@@ -14,6 +13,10 @@ process.on("unhandledRejection", (reason, promise) => {
 
 bootstrap();
 
+// TODO: usar Pino para logs
+// TODO: paralelisar servidor com uso de workers (adicionar uma var no .env para controle)
+// TODO: criar um utils para obter o .env (tipar e validar)
+// TODO: adicionar camada de erro na aplicação
 async function bootstrap() {
     await connectToMongoDB();
     createHttpServer();
@@ -23,10 +26,9 @@ function createHttpServer() {
     const app = express()
     const port = 3000
     app.use(express.json());
-    app.use(globalExpressHandle)
-    app.use(routes)
+    app.use(routes);
 
     app.listen(port, () => {
-    console.log(`App de exemplo esta rodando na porta ${port}`)
+        console.log(`App de exemplo esta rodando na porta ${port}`)
     })
 }
